b7261da2e23190cc0ba54930aa2539613cc83156
fabianfa <fabianfa.munoz@gmail.com>
2022-09-11 20:57:36 -0500

    Add, test message

--- src/index.js
+++ src/index.js
@@ -19,6 +19,12 @@ app.use(express.json())
 app.use('/user',require('./routers/user'))
 app.use('/products',require('./routers/products'))
 
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        "status": "The API is working correctly"
+    })
+})
+
 app.listen(PORT,()=>{
     console.log(`Api initialized on port ${PORT}`)
 })
